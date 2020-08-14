bb06395a3c5f0414037d55d35ea1d690f7c796bc
jenniekibiri <josphatje@gmail.com>
2020-08-15 00:08:36 +0300

    place players on the grid

--- app/js/map.js
+++ app/js/map.js
@@ -14,12 +14,14 @@
  }
  createMap(100)
 
+
 function weapons(name,image,damage){
 this.name=name;
 this.image=image;
 this.damage=damage;
 
 }
+
 weapons.prototype.setWeaponposition=function(){
 cell=availableCell()
 arr[cell]=this.name
@@ -27,15 +29,61 @@ const weaponBox=document.getElementById(cell)
 weaponBox.classList.add(this.name)
 }
 
+function player(name,image){
+    this.name =name;
+    this.image=image
+}
+//position the players
+player.prototype.setPlayerPosition=function(){
+    cell=availableCell()
+    arr[cell]=this.name
+    const playerBox = document.getElementById(cell)
+    playerBox.classList.add(this.name)
+    //players should not be adjacent
+    var contacts= [cell-1,cell-10,cell+1,cell+10];
+
+    contacts.forEach((contact,i)=>{
+        if((contact >=0 && contact<100) && arr[contact]==null)
+        arr[contact]='full'
+    })
+    return this.position = cell;
+}
+
+
+function obstacles(name,image){
+    this.name=name;
+    this.image=image
+}
+obstacles.prototype.setObstaclePosition=function(){
+    for(i=0; i<10;i++){
+        cell=availableCell()
+        arr[cell]=this.name
+        const obstacleBox = document.getElementById(cell)
+        obstacleBox.classList.add(this.name)  
+    }
+}
+
+//instantiate obstacles
+const obstacle =new obstacles('obstacle','obstacle.png')
+//instantiate players
+const player1 = new player('player1','player1.png')
+const player2 = new player('player2','player2.png')
+
 //instantiate the objects
 const sword = new weapons('sword','sword.png',20)
 const dagger = new weapons('dagger','dagger.png',12)
 const gun= new weapons('gun','gun.png',15)
 const bow = new weapons('bow','bow.png',10)
+
+
 sword.setWeaponposition()
 dagger.setWeaponposition()
 gun.setWeaponposition()
 bow.setWeaponposition()
+player1.setPlayerPosition()
+player2.setPlayerPosition()
+obstacle.setObstaclePosition()
+
 //findAvailableCell
 function availableCell(){
     do{
